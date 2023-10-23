87eed50698e01e6e947964d09615860573c26bb7
Matee <matiullahsafibbc@gmail.com>
2023-10-23 11:36:56 +0430

    update SEO components

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Afghan Travel Agency`,
+    siteTitleAlt: `Afghan Travel Agency`,
+    siteHeadline: `Afghan Travel Agency`,
+    siteUrl: `https://afghantravelagency.com`,
+    siteDescription: `Your travel guide in Afghanistan. We offer visa services for afghans willing to travel abroad. We also offer computer services for filling and submitting forms for visa applications, passport applications, and ID application froms.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `Matee Safi`,
   },
   trailingSlash: `always`,
   plugins: [
